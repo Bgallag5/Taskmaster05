bf1be60dee6c571691858c3515e28854c3e292a9
bengallagher <bgallag5@gmail.com>
2021-06-20 12:02:45 -0400

    moment task update

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
-
+  auditTask(taskLi);
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -43,6 +43,25 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }   
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 //
@@ -95,15 +114,21 @@ $(".list-group").on("click", "span", function() {
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
-
+    $(this).replaceWith(dateInput);
   // swap out elements
-  $(this).replaceWith(dateInput);
+  
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function(){
+      $(this).trigger("change")
+    }
+  });
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -131,6 +156,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -185,10 +211,10 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event) {
+  activate: function(event, ui) {
     console.log("activate", this);
   },
-  deactivate: function(event) {
+  deactivate: function(event, ui) {
     console.log("deactivate", this);
   },
   over: function(event) {
@@ -246,3 +272,14 @@ $("#trash").droppable({
     console.log("out");
   }
 });
+//////////////////////////////date picker///////
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+////////////////////////////////////////////MOMENT.js time events/////////
+
+
+
+
+
+//64 .trim, 
